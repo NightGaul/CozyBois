84d82a30ff5aea75bd57ddcde10bfa4e400f568f
NightGaul <marina.willi@bluemail.ch>
2024-03-26 19:59:13 +0100

    Rotation and Interaction implemented

--- Assets/Code/Interactions/DandelionController.cs
+++ Assets/Code/Interactions/DandelionController.cs
@@ -1,60 +1,43 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Code.Helpers;
 using UnityEngine;
 
 public class DandelionController : MonoBehaviour
 {
-    //private ParticleSystem _particleSystem;
-    // Start is called before the first frame update
-
     private ParticleSystem _ps;
-
-    public int numParticles = 64;
-
-    // these lists are used to contain the particles which match
-    // the trigger conditions each frame.
-    List<ParticleSystem.Particle> _enter = new List<ParticleSystem.Particle>();
     private ParticleSystem.EmitParams _emitParams;
-    
+    private List<ParticleSystem.Particle> _enter = new List<ParticleSystem.Particle>();
 
+    private readonly int _numParticles = 64;
     private Quaternion _playerRotation;
-
 
     void Start()
     {
-        Vector3[] positions = PointsOnSphere(64);
+        Vector3[] positions = VectorModifiers.PointsOnSphere(64);
+        float scaling = 0.075f;
 
         _emitParams = new ParticleSystem.EmitParams();
         _ps = GetComponent<ParticleSystem>();
 
-        float scaling = 0.075f;
 
-        for (int i = 0; i < numParticles; i++)
+        for (int i = 0; i < _numParticles; i++)
         {
             // Set the position of the emitted particle
             Vector3 particlePosition = positions[i] * scaling;
             _emitParams.position = particlePosition;
-
-            _emitParams.rotation3D = CalculateRotation(particlePosition, Vector3.zero).eulerAngles * -1;
-            
+            _emitParams.rotation3D = VectorModifiers.CalculateRotation(particlePosition, Vector3.zero).eulerAngles * -1;
             _ps.Emit(_emitParams, 1);
         }
     }
 
-    private Vector3 PointRotation(Vector3 P1, Vector3 P2, Quaternion rot)
-    {
-        var v = P1 - P2; //the relative vector from P2 to P1.
-        v = rot * v; //rotatate
-        v = P2 + v; //bring back to world space
-
-        return v;
-    }
 
     void OnParticleTrigger()
     {
         // get the particles which matched the trigger conditions this frame
         if (_ps == null) return;
+        
         int numEnter = _ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, _enter);
 
 
@@ -67,69 +50,38 @@ public class DandelionController : MonoBehaviour
 
 
             Vector3 position;
-           
             var position1 = gameObject.transform.position;
-            position = PointRotation(position1 + p.position, position1, _playerRotation);
+            position = VectorModifiers.PointRotation(position1 + p.position, position1, _playerRotation);
+            Debug.Log(_ps.trigger.GetCollider(i));
+            try
+            {
+                position = new Vector3(position.x, _ps.trigger.GetCollider(i).transform.position.y, position.z);
+            }
+            catch
+            {
+                Debug.Log("oh no");
+            }
 
-
-            position = new Vector3(position.x, _ps.trigger.GetCollider(i).transform.position.y, position.z);
-            
             ball.transform.position = position;
-
             ball.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
 
             _enter[i] = p;
-            
         }
 
         _ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, _enter);
     }
-    public void Blow(Quaternion rotationPlayer)
+
+    public void  Blow(Quaternion rotationPlayer, float speed)
     {
         Debug.Log("i'm being blown");
-        _playerRotation = rotationPlayer;
+
+        float _speed = speed/50;
+        if (_speed < 1) _speed = 1;
         var velocityOverLifetimeModule = _ps.velocityOverLifetime;
-        velocityOverLifetimeModule.speedModifier = new ParticleSystem.MinMaxCurve(0.05f,0.2f);
+        _playerRotation = rotationPlayer;
+        velocityOverLifetimeModule.speedModifier = new ParticleSystem.MinMaxCurve(_speed-0.8f, _speed+0.8f);
         GetComponentsInParent<Transform>()[1].rotation = rotationPlayer;
-
         
         
-
-
-    }
-    
-    
-
-    Vector3[] PointsOnSphere(int n)
-    {
-        List<Vector3> upts = new List<Vector3>();
-        float inc = Mathf.PI * (3 - Mathf.Sqrt(5));
-        float off = 2.0f / n;
-        float x = 0;
-        float y = 0;
-        float z = 0;
-        float r = 0;
-        float phi = 0;
-
-        for (var k = 0; k < n; k++)
-        {
-            y = k * off - 1 + (off / 2);
-            r = Mathf.Sqrt(1 - y * y);
-            phi = k * inc;
-            x = Mathf.Cos(phi) * r;
-            z = Mathf.Sin(phi) * r;
-
-            upts.Add(new Vector3(x, y, z));
-        }
-
-        Vector3[] pts = upts.ToArray();
-        return pts;
-    }
-
-    public Quaternion CalculateRotation(Vector3 point1, Vector3 point2)
-    {
-        Vector3 direction = point2 - point1;
-        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, direction);
-        return rotation;
     }
 }
