5536dc9594df56685d897116ef5b2464a2a19366
NightGaul <marina.willi@bluemail.ch>
2024-04-05 15:10:08 +0200

    Basic Terrain

---
+++ Assets/Code/Editors/TerrainCustomEditor.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(RaiseTerrainHeightmap))]
+public class TerrainCustomEditor : Editor
+{
+
+
+    // public RaiseHeights raiseHeights;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        RaiseTerrainHeightmap RaiseTerrain = (RaiseTerrainHeightmap)target;
+        if (GUILayout.Button("Raise Terrain Heightmap"))
+        { RaiseTerrain.RaiseHeights(); }
+    }
+}
