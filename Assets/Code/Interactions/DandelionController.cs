836b070893ddc291a8b4947d431c975087f2fdbf
NightGaul <marina.willi@bluemail.ch>
2024-04-11 14:17:10 +0200

    Trees Bridges etc

--- Assets/Code/Interactions/DandelionController.cs
+++ Assets/Code/Interactions/DandelionController.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Code.Helpers;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using Object = System.Object;
 using Random = UnityEngine.Random;
 
 public class DandelionController : MonoBehaviour
@@ -23,8 +25,7 @@ public class DandelionController : MonoBehaviour
 
         _emitParams = new ParticleSystem.EmitParams();
         _ps = GetComponent<ParticleSystem>();
-
-
+        
         for (int i = 0; i < _numParticles; i++)
         {
             // Set the position of the emitted particle
@@ -47,7 +48,7 @@ public class DandelionController : MonoBehaviour
         for (int i = 0; i < numEnter; i++)
         {
             ParticleSystem.Particle p = _enter[i];
-            p.startColor = new Color32(255, 0, 0, 255);
+            //p.startColor = new Color32(255, 0, 0, 255);
             //GameObject ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 
 
@@ -63,81 +64,56 @@ public class DandelionController : MonoBehaviour
             }
             catch
             {
-                //oh no;
+                /*this bitch empty*/
             }
-
 
             //position tells us the position of the dandelion seed
             var temp = Random.Range(0, 10);
-            //Debug.Log(temp);
-            if (temp < 4)
-            {
-                SpawnNewDandelion(position);
-                //Debug.Log(position);
-            }
-
-
+            if (temp < 4) SpawnNewDandelion(position);
+            
             _enter[i] = p;
         }
-
+        
         _ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, _enter);
     }
 
     public void Blow(Quaternion rotationPlayer, float speed)
     {
-        // Debug.Log("i'm being blown");
-
-        float _speed = speed / 50;
-        if (_speed < 1) _speed = 1;
+        float _speed = speed / 40;
+        if (_speed < 0.5) _speed = 0.5f;
         var velocityOverLifetimeModule = _ps.velocityOverLifetime;
         _playerRotation = rotationPlayer;
         velocityOverLifetimeModule.speedModifier = new ParticleSystem.MinMaxCurve(_speed - 0.8f, _speed + 0.8f);
-        var rotationPlayerEuler = rotationPlayer.eulerAngles;
-        var vectorQuaternion = new Vector3(-15, rotationPlayerEuler.y, rotationPlayerEuler.z);
         transform.rotation = rotationPlayer;
-        //Quaternion.Euler(vectorQuaternion);
-        //GetComponentsInParent<Transform>()[1].rotation = Quaternion.Euler(vectorQuaternion);
+        //var mainModule = _ps.main;
+        //mainModule.startLifetime = 15;
+        //StartCoroutine(WaitDandelion());
     }
 
     private void SpawnNewDandelion(Vector3 position)
     {
-        // RaycastHit hit;
-        // Ray ray = new Ray(position, Vector3.down);
-        // Physics.Raycast(ray, out hit);
-        // Debug.Log((hit.collider.transform.position));
-        // position = hit.collider.transform.position;
-        //Debug.Log("SpawnNew");
-        var temp = Instantiate(_dandelionPrefab, new Vector3(position.x, transform.position.y- 2f, position.z), Quaternion.identity);
+        var temp = Instantiate(_dandelionPrefab, new Vector3(position.x, transform.position.y - 2f, position.z), Quaternion.identity); //
         temp.GetComponentInChildren<ParticleSystem>().collision.SetPlane(0, GameObject.Find("Terrain").transform);
-        temp.GetComponentInChildren<ParticleSystem>().collision.SetPlane(0, GameObject.Find("Terrain_(0.00, 0.00, 1000.00)").transform);
-        temp.GetComponentInChildren<ParticleSystem>().collision.SetPlane(0, GameObject.Find("Terrain_(1000.00, 0.00, 0.00)").transform);
+        temp.GetComponentInChildren<ParticleSystem>().collision
+            .SetPlane(0, GameObject.Find("Terrain_(0.00, 0.00, 1000.00)").transform);
+        temp.GetComponentInChildren<ParticleSystem>().collision
+            .SetPlane(0, GameObject.Find("Terrain_(1000.00, 0.00, 0.00)").transform);
         var velocityOverLifetimeModule = temp.GetComponentInChildren<ParticleSystem>().velocityOverLifetime;
-        velocityOverLifetimeModule.speedModifier = new ParticleSystem.MinMaxCurve(0,0);
-        temp.GetComponentInChildren<ParticleSystem>().trigger
-            .SetCollider(0, GameObject.Find("Terrain").transform);
-        temp.GetComponentInChildren<ParticleSystem>().trigger
-            .SetCollider(0, GameObject.Find("Terrain_(0.00, 0.00, 1000.00)").transform);
-        temp.GetComponentInChildren<ParticleSystem>().trigger
-            .SetCollider(0, GameObject.Find("Terrain_(1000.00, 0.00, 0.00)").transform);
+        velocityOverLifetimeModule.speedModifier = new ParticleSystem.MinMaxCurve(0, 0);
+        // temp.GetComponentInChildren<ParticleSystem>().trigger
+        //     .SetCollider(0, GameObject.Find("Terrain").transform);
+        // temp.GetComponentInChildren<ParticleSystem>().trigger
+        //     .SetCollider(0, GameObject.Find("Terrain_(0.00, 0.00, 1000.00)").transform);
+        // temp.GetComponentInChildren<ParticleSystem>().trigger
+        //     .SetCollider(0, GameObject.Find("Terrain_(1000.00, 0.00, 0.00)").transform);
         temp.GetComponentInChildren<DandelionController>()._dandelionPrefab = _dandelionPrefab;
-        
-        
-        //If we want the dandelions to grow from the floor
-        // temp.transform.localScale = new Vector3(0.1f,0.1f,0.1f);
-        // for (int i = 0; i < 99; i++)
-        // {
-        //     
-        //     
-        //     StartCoroutine(Wait(0.5f , temp));
-        // }
-        // //Debug.Log("spawn new");
     }
-    
-    // IEnumerator Wait(float time, GameObject temp)
-    // {
-    //     
-    //     yield return new WaitForSecondsRealtime(time);
-    //     temp.transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
-    //     
-    // }
+
+    IEnumerator WaitDandelion()
+    {
+        //size seeds down
+        yield return new WaitForSeconds(40f);
+        Debug.Log("self destroy");
+        GameObject.Destroy(gameObject);
+    }
 }
